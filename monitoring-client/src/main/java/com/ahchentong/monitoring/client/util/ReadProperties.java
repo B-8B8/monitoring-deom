7369c164e75a492110f4e69c8e5c3d12656b0814
骆昊宇 <i.o.w@outlook.com>
2019-05-24 16:46:07 +0800

    修改部分代码

---
+++ monitoring-client/src/main/java/com/ahchentong/monitoring/client/util/ReadProperties.java
@@ -0,0 +1,55 @@
+package com.ahchentong.monitoring.client.util;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+public class ReadProperties {
+
+    private static final String CONFIG = "server.ip=localhost\nserver.port=65535";
+    private static final String FILE_NAME = "monitoring.client.config.properties";
+    private static final String SYSTEM_GET_PROPERTY = "path.separator";
+    private static final String PARAMETER_1 = "server.ip";
+    private static final String PARAMETER_2 = "server.port";
+
+    public String[] read() {
+        String path = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        int firstIndex = path.lastIndexOf(System.getProperty(SYSTEM_GET_PROPERTY)) + 1;
+        int lastIndex = path.lastIndexOf(File.separator) + 1;
+        path = path.substring(firstIndex, lastIndex);
+        File file = new File(path + FILE_NAME);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            BufferedWriter bufferedWriter = null;
+            try {
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, false), StandardCharsets.UTF_8));
+                bufferedWriter.write(CONFIG);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (bufferedWriter != null) {
+                        bufferedWriter.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (file.exists()) {
+            Properties properties = new Properties();
+            try {
+                properties.load(new FileInputStream(path + FILE_NAME));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return new String[]{properties.getProperty(PARAMETER_1),properties.getProperty(PARAMETER_2)};
+        }
+        return null;
+    }
+}
